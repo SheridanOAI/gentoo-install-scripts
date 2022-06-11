131a454fbce4aff7970ef422d652b6d0bf13745b
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2022-06-11 12:23:13 +0300

    Update gen-inst-prog-kde.sh

--- gen-inst-prog-kde.sh
+++ gen-inst-prog-kde.sh
@@ -1,9 +1,11 @@
- #!/bin/bash
+#!/bin/bash
+echo 'app-admin/conky imlib curl lua bundled-toluapp rss nvidia ABI_X86=(64) LUA_SINGLE_TARGET="lua5-3"' >> /etc/portage/package.use/conky
+echo "media-plugins/audacious-plugins cue ffmpeg wavpack lame soxr" >> /etc/portage/package.use/Audacious
+echo "sys-apps/inxi hddtemp" >> /etc/portage/package.use/inxi
 
-
- TOOLS="sys-fs/f2fs-tools sys-fs/dosfstools kde-apps/ark app-arch/p7zip app-arch/unrar app-dicts/aspell-ru"
- FONTS="dev-perl/Font-TTF media-fonts/font-bh-ttf media-fonts/font-misc-meltho media-fonts/font-misc-misc media-fonts/font-adobe-utopia-type1 media-fonts/font-adobe-utopia-100dpi media-fonts/font-adobe-utopia-75dpi media-fonts/ubuntu-font-family"
- PACKAGES="media-sound/audacious app-office/libreoffice-bin sys-apps/gnome-disk-utility media-sound/kid3 media-sound/soundconverter media-sound/flacon kde-apps/kcalc games-board/gnome-mahjongg app-admin/conky media-sound/pavucontrol-qt kde-apps/gwenview media-gfx/flameshot kde-misc/latte-dock net-p2p/qbittorrent"
+TOOLS="sys-fs/f2fs-tools sys-fs/dosfstools kde-apps/ark app-arch/p7zip app-arch/unrar app-dicts/aspell-ru sys-apps/inxi app-misc/neofetch"
+FONTS="dev-perl/Font-TTF media-fonts/font-bh-ttf media-fonts/font-misc-meltho media-fonts/font-misc-misc media-fonts/font-adobe-utopia-type1 media-fonts/font-adobe-utopia-100dpi media-fonts/font-adobe-utopia-75dpi media-fonts/ubuntu-font-family media-fonts/noto-emoji"
+PACKAGES="media-sound/audacious sys-apps/gnome-disk-utility media-sound/kid3 media-sound/soundconverter media-sound/flacon kde-apps/kcalc games-board/gnome-mahjongg app-admin/conky media-sound/pavucontrol-qt kde-apps/gwenview media-gfx/flameshot kde-misc/latte-dock net-p2p/qbittorrent"
 
     echo "Установка утилит"
 emerge --ask $TOOLS
@@ -13,4 +15,3 @@ emerge --ask $FONTS
 emerge --ask $PACKAGES
 etc-update --automode -3
 emerge --ask $PACKAGES
-
