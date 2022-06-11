8736d3b4eb89f3ce49253cc54be6e2e90ef00b85
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2022-06-11 12:22:18 +0300

    Update gen-inst-prog-gtk.sh

--- gen-inst-prog-gtk.sh
+++ gen-inst-prog-gtk.sh
@@ -1,11 +1,15 @@
- #!/bin/bash
+#!/bin/bash
 
- TOOLS="sys-fs/f2fs-tools sys-fs/dosfstools app-arch/p7zip app-arch/unrar app-dicts/aspell-ru"
- FONTS="dev-perl/Font-TTF media-fonts/font-bh-ttf media-fonts/font-misc-meltho media-fonts/font-misc-misc media-fonts/font-adobe-utopia-type1 media-fonts/font-adobe-utopia-100dpi media-fonts/font-adobe-utopia-75dpi media-fonts/ubuntu-font-family"
- PACKAGES="media-sound/audacious app-office/libreoffice-bin sys-apps/gnome-disk-utility media-sound/kid3 media-sound/soundconverter media-sound/flacon games-board/gnome-mahjongg app-admin/conky media-sound/pavucontrol media-gfx/flameshot net-p2p/qbittorrent"
+    echo 'app-admin/conky imlib curl lua bundled-toluapp rss nvidia ABI_X86=(64) LUA_SINGLE_TARGET="lua5-3"' >> /etc/portage/package.use/conky
+    echo "media-plugins/audacious-plugins cue ffmpeg wavpack lame soxr" >> /etc/portage/package.use/Audacious
+    echo "sys-apps/inxi hddtemp" >> /etc/portage/package.use/inxi
 
-echo 'app-admin/conky imlib curl lua bundled-toluapp rss nvidia ABI_X86=(64) LUA_SINGLE_TARGET="lua5-3"' >> /etc/portage/package.use/conky
-echo "media-plugins/audacious-plugins cue ffmpeg wavpack lame soxr" >> /etc/portage/package.use/audacious
+    TOOLS="sys-fs/f2fs-tools sys-fs/dosfstools app-arch/p7zip app-arch/unrar app-dicts/aspell-ru"
+    FONTS="dev-perl/Font-TTF media-fonts/font-bh-ttf media-fonts/font-misc-meltho media-fonts/font-misc-misc media-fonts/font-adobe-utopia-type1 media-fonts/font-adobe-utopia-100dpi media-fonts/font-adobe-utopia-75dpi media-fonts/ubuntu-font-family media-fonts/noto-emoji gnome-extra/gucharmap"
+    PACKAGES="media-sound/audacious sys-apps/gnome-disk-utility media-sound/kid3 media-sound/soundconverter media-sound/flacon games-board/gnome-mahjongg media-gfx/nomacs app-admin/conky media-sound/pavucontrol media-gfx/flameshot net-p2p/qbittorrent"
+
+    echo 'app-admin/conky imlib curl lua bundled-toluapp rss nvidia ABI_X86=(64) LUA_SINGLE_TARGET="lua5-3"' >> /etc/portage/package.use/conky
+    echo "media-plugins/audacious-plugins cue ffmpeg wavpack lame soxr" >> /etc/portage/package.use/audacious
 
     echo "Установка утилит"
 emerge --ask $TOOLS
@@ -13,6 +17,5 @@ emerge --ask $TOOLS
 emerge --ask $FONTS
     echo "Установка программ"
 emerge --ask $PACKAGES
-#etc-update --automode -3
-#emerge --ask $PACKAGES
-
+etc-update --automode -3
+emerge --ask $PACKAGES
