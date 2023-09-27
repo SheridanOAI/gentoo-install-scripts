746338c3da99bd7ec7464843c4f56e57f9d3840a
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2023-09-27 18:50:53 +0300

    Update gen-inst-prog-gtk.sh

--- gen-inst-prog-gtk.sh
+++ gen-inst-prog-gtk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo '01. Установка дополнительных флагов для Nvidia Conky'
-echo 'app-admin/conky imlib curl lua bundled-toluapp rss nvidia ABI_X86=(64) LUA_SINGLE_TARGET="lua5-3"' >> /etc/portage/package.use/conky
+echo '01. Установка дополнительных флагов для Conky'
+echo 'app-admin/conky imlib curl lua bundled-toluapp rss ABI_X86=(64) LUA_SINGLE_TARGET="lua5-3"' >> /etc/portage/package.use/conky
 
 echo '02. Установка дополнительных флагов для Audacious'
 echo "media-plugins/audacious-plugins cue ffmpeg wavpack lame soxr" >> /etc/portage/package.use/Audacious
@@ -18,9 +18,8 @@ media-fonts/font-misc-misc media-fonts/font-adobe-utopia-type1 \
 media-fonts/font-adobe-utopia-100dpi media-fonts/font-adobe-utopia-75dpi
 media-fonts/ubuntu-font-family media-fonts/noto-emoji gnome-extra/gucharmap"
 
-export PACKAGES="media-sound/audacious sys-apps/gnome-disk-utility media-sound/kid3 \
-media-sound/soundconverter media-sound/flacon games-board/gnome-mahjongg \
-media-gfx/nomacs app-admin/conky media-sound/pavucontrol net-p2p/qbittorrent"
+export PACKAGES="sys-apps/gnome-disk-utility games-board/gnome-mahjongg \
+app-admin/conky media-sound/pavucontrol"
 
 echo "04. Установка утилит"
 emerge --ask $TOOLS
