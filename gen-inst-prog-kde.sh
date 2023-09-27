840f205cc910103823a28bdcc9bbeb59a1ecc768
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2023-09-27 18:46:38 +0300

    Update gen-inst-prog-kde.sh

--- gen-inst-prog-kde.sh
+++ gen-inst-prog-kde.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo '01. Установка дополнительных флагов для Nvidia Conky'
-echo 'app-admin/conky imlib curl lua bundled-toluapp rss nvidia ABI_X86=(64) LUA_SINGLE_TARGET="lua5-3"' >> /etc/portage/package.use/conky
+echo '01. Установка дополнительных флагов для Conky'
+echo 'app-admin/conky imlib curl lua bundled-toluapp rss ABI_X86=(64) LUA_SINGLE_TARGET="lua5-3"' >> /etc/portage/package.use/conky
 
 echo '02. Установка дополнительных флагов для Audacious'
 echo "media-plugins/audacious-plugins cue ffmpeg wavpack lame soxr" >> /etc/portage/package.use/Audacious
@@ -10,16 +10,16 @@ echo '03. Установка дополнительных флагов для In
 echo "sys-apps/inxi hddtemp" >> /etc/portage/package.use/inxi
 
 export TOOLS="sys-fs/f2fs-tools sys-fs/dosfstools kde-apps/ark app-arch/p7zip \
-app-arch/unrar app-dicts/aspell-ru sys-apps/inxi app-misc/neofetch sys-apps/nvme-cli"
+app-arch/unrar app-dicts/aspell-ru app-misc/neofetch sys-apps/nvme-cli"
 
 export FONTS="dev-perl/Font-TTF media-fonts/font-bh-ttf media-fonts/font-misc-meltho \
 media-fonts/font-misc-misc media-fonts/font-adobe-utopia-type1 \
 media-fonts/font-adobe-utopia-100dpi media-fonts/font-adobe-utopia-75dpi \
 media-fonts/ubuntu-font-family media-fonts/noto-emoji"
 
-export PACKAGES="media-sound/audacious sys-apps/gnome-disk-utility media-sound/kid3 \
+export PACKAGES="media-sound/audacious sys-apps/gnome-disk-utility kde-apps/dolphin media-sound/kid3 \
 media-sound/soundconverter media-sound/flacon kde-apps/kcalc games-board/gnome-mahjongg \
-app-admin/conky media-sound/pavucontrol-qt kde-apps/gwenview media-gfx/flameshot \
+app-admin/conky media-sound/pavucontrol-qt kde-apps/gwenview www-client/firefox-bin \
 net-p2p/qbittorrent kde-apps/kdenlive"
 
 echo "04. Установка утилит"
