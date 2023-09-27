9b6fd9319714d1de26f9989cc5f8adc86b23633f
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2023-09-27 18:55:37 +0300

    Update gen1.sh

--- gen1.sh
+++ gen1.sh
@@ -12,13 +12,9 @@ export DATA2_LOCATION=/mnt/gentoo/home/data2
 export GAMES_LOCATION=/mnt/gentoo/home/games
 
 #echo 'Выбор файла make.conf для NVIDIA или AMD'
-export MAKE_KDE_NV=/mnt/gentoo/gentoo-install-scripts-main/make_kde_nv.conf
-export MAKE_GNOME_NV=/mnt/gentoo/gentoo-install-scripts-main/make_gnome_nv.conf
-export MAKE_CINNAMON_NV=/mnt/gentoo/gentoo-install-scripts-main/make_cinnamon_nv.conf
-export MAKE_NV=/mnt/gentoo/gentoo-install-scripts-main/make_nv.conf
 export MAKE_AMD=/mnt/gentoo/gentoo-install-scripts-main/make_amd.conf
-
-
+export MAKE_NV=/mnt/gentoo/gentoo-install-scripts-main/make_nv.conf
+export MAKE_INTEL=/mnt/gentoo/gentoo-install-scripts-main/make_intel.conf
 
 echo '01. Выбор раздела ROOT (/dev/xxx)'
 read -p 'DEV_' DEV_
@@ -28,11 +24,10 @@ echo '1 - BTRFS, 2 - EXT4'
 read choice
 
 if [[ "$choice" == "1" ]]; then
-    mkfs.btrfs -L Gentoo -f $DEV_ && mount $DEV_ /mnt/gentoo && \
-    cd /mnt/gentoo && btrfs sub cre @ && btrfs sub cre @home && \
-    btrfs sub cre @cache && btrfs sub cre @log && cd / && umount /mnt/gentoo
+    mkfs.btrfs -L Gentoo -f $DEV_ && mkdir /mnt/gentoo && mount $DEV_ /mnt/gentoo && \
+    cd /mnt/gentoo && btrfs sub cre @ && cd / && umount /mnt/gentoo
 elif [[ "$choice" == "2" ]]; then
-    mkfs.ext4 -L Gentoo $DEV_ && mount $DEV_ /mnt/gentoo && \
+    mkfs.ext4 -L Gentoo $DEV_ && mkdir /mnt/gentoo && mount $DEV_ /mnt/gentoo && \
     mkdir -p /mnt/gentoo/home/{data,data2,games} && mkdir -p /mnt/gentoo/boot/efi && \
     cd / && umount /mnt/gentoo
 fi
@@ -42,13 +37,9 @@ echo '1 - BTRFS, 2 - EXT4'
 read choice
 
 if [[ "$choice" == "1" ]]; then
-    mount -o noatime,autodefrag,compress=zstd,subvol=@ $DEV_ /mnt/gentoo && \
+    mount -o rw,noatime,ssd,discard=async,space_cashe=v2,compress=zstd,subvol=@ $DEV_ /mnt/gentoo && \
     mkdir -p /mnt/gentoo/home/{data,data2/games} && mkdir -p /mnt/gentoo/boot/efi && \
-    mkdir -p /mnt/gentoo/var/log && mkdir -p /mnt/gentoo/var/cache && \
-    mount -o noatime,autodefrag,compress=zstd,subvol=@home $DEV_ /mnt/gentoo/home && \
-    mount -o noatime,autodefrag,compress=zstd,subvol=@cache $DEV_ /mnt/gentoo/var/cache && \
-    mount -o noatime,autodefrag,compress=zstd,subvol=@log $DEV_ /mnt/gentoo/var/log
-elif [[ "$choice" == "2" ]]; then
+    elif [[ "$choice" == "2" ]]; then
     mount $DEV_ /mnt/gentoo
 fi
 
@@ -89,19 +80,15 @@ tar xpvf stage3*.tar.xz --xattrs-include='*.*' --numeric-owner
 echo '13. Копируем make.conf для нужного оборудования и DE'
 wget https://github.com/SheridanOAI/gentoo-install-scripts/archive/refs/heads/main.zip
 unzip main.zip -d /mnt/gentoo
-echo '1-MAKE.CONF-KDE-NV, 2-MAKE.CONF-GNOME-NV, 3-MAKE.CONF-CINNAMON-NV, 4-MAKE.CONF-NV, 5-MAKE.CONF-AMD'
+echo '1-MAKE-AMD, 2-MAKE-NVIDIA, 3-MAKE-INTEL'
 read choice
 
 if [[ "$choice" == "1" ]]; then
-    MAKE_CONF=$MAKE_KDE_NV
-elif [[ "$choice" == "2" ]]; then
-    MAKE_CONF=$MAKE_GNOME_NV
-elif [[ "$choice" == "3" ]]; then
-    MAKE_CONF=$MAKE_CINNAMON_NV
-elif [[ "$choice" == "4" ]]; then
-    MAKE_CONF=$MAKE_NV
-elif [[ "$choice" == "5" ]]; then
     MAKE_CONF=$MAKE_AMD
+elif [[ "$choice" == "2" ]]; then
+    MAKE_CONF=$MAKE_NV
+elif [[ "$choice" == "3" ]]; then
+    MAKE_CONF=$MAKE_INTEL
 fi
 
 cp $MAKE_CONF /mnt/gentoo/etc/portage/make.conf
@@ -110,7 +97,7 @@ echo '14. Создаём каталог repos.conf'
 mkdir --parents /mnt/gentoo/etc/portage/repos.conf
 
 echo '15. Копируем конфигурацию репозитория Gentoo в gentoo.conf'
-cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+cp /usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 
 echo '16. Копирование информации о DNS'
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
