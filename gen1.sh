c1be4c7ec1dca28d76036ac08d15deb33e9b8d7c
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2024-04-10 19:13:48 +0300

    Update gen1.sh

--- gen1.sh
+++ gen1.sh
@@ -5,11 +5,11 @@ path2=/
 setfont UniCyr_8x16
 
 #echo 'Выбор места установки разделов (LOCATION)'
-export ROOT_LOCATION=/mnt/gentoo
-export BOOT_LOCATION=/mnt/gentoo/boot/efi/
-export DATA_LOCATION=/mnt/gentoo/home/data
+export  ROOT_LOCATION=/mnt/gentoo
+export  BOOT_LOCATION=/mnt/gentoo/boot/efi
+export  DATA_LOCATION=/mnt/gentoo/home/data
 export DATA2_LOCATION=/mnt/gentoo/home/data2
-export GAMES_LOCATION=/mnt/gentoo/home/games
+export DATA3_LOCATION=/mnt/gentoo/home/data3
 
 #echo 'Выбор файла make.conf для NVIDIA или AMD'
 export MAKE_AMD=/mnt/gentoo/gentoo-install-scripts-main/make_amd.conf
@@ -24,7 +24,7 @@ echo '1 - BTRFS, 2 - EXT4'
 read choice
 
 if [[ "$choice" == "1" ]]; then
-    mkfs.btrfs -L Gentoo -f $DEV_ && mkdir -p /mnt/gentoo && mount $DEV_ /mnt/gentoo && \
+    mkfs.btrfs -L Gentoo -f $DEV_ && mkdir /mnt/gentoo && mount $DEV_ /mnt/gentoo && \
     cd /mnt/gentoo && btrfs sub cre @ && cd / && umount /mnt/gentoo
 elif [[ "$choice" == "2" ]]; then
     mkfs.ext4 -L Gentoo $DEV_ && mkdir /mnt/gentoo && mount $DEV_ /mnt/gentoo && \
@@ -37,9 +37,9 @@ echo '1 - BTRFS, 2 - EXT4'
 read choice
 
 if [[ "$choice" == "1" ]]; then
-    mount -o rw,noatime,ssd,discard=async,compress=zstd,subvol=@ $DEV_ /mnt/gentoo && \
-    mkdir -p /mnt/gentoo/home/{data,data2/games} && mkdir -p /mnt/gentoo/boot/efi
-elif [[ "$choice" == "2" ]]; then
+    mount -o rw,noatime,ssd,discard=async,space_cashe=v2,compress=zstd,subvol=@ $DEV_ /mnt/gentoo && \
+    mkdir -p /mnt/gentoo/home/{data,data2/games} && mkdir -p /mnt/gentoo/boot/efi && \
+    elif [[ "$choice" == "2" ]]; then
     mount $DEV_ /mnt/gentoo
 fi
 
@@ -55,9 +55,9 @@ echo '06. Монтирование раздела DATA2'
 read -p 'DATA2_PARTITION_' DATA2_PARTITION_
 mount $DATA2_PARTITION_ $DATA2_LOCATION
 
-echo '07. Монтирование раздела Games'
-read -p 'GAMES_PARTITION_' GAMES_PARTITION_
-mount $GAMES_PARTITION_ $GAMES_LOCATION
+echo '07. Монтирование раздела DATA3'
+read -p 'DATA3_PARTITION_' DATA3_PARTITION_
+mount $DATA3_PARTITION_ $DATA3_LOCATION
 
 echo '08. Монтирование раздела SWAP'
 read -p 'SWAP_PARTITION_' SWAP_PARTITION_
@@ -67,8 +67,8 @@ echo '09. Переходим в корень устанавливаемой си
 cd $path1
 
 echo '10. Скачиваем архив stage3 (парсинг stage3-amd64-openrc)'
-a=$(curl https://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds/current-stage3-amd64-openrc/ | sed -e 's/<[^>]*>//g' |cut -f1 -d' '| grep -e "^stage3-amd64-openrc.*.tar.xz$")
-path_to_stage3='https://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds/current-stage3-amd64-openrc/'$a
+a=$(curl https://gentoo-mirror.alexxy.name/releases/amd64/autobuilds/current-stage3-amd64-openrc/ | sed -e 's/<[^>]*>//g' |cut -f1 -d' '| grep -e "^stage3-amd64-openrc.*.tar.xz$")
+path_to_stage3='https://gentoo-mirror.alexxy.name/releases/amd64/autobuilds/current-stage3-amd64-openrc/'$a
 wget $path_to_stage3 -O stage3-amd64-openrc.tar.xz
 
 echo '11. Копируем архив stage3'
